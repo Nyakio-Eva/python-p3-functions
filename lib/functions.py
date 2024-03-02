9c1f94070d3d98f5c9ecbbb3f0b554b86ca9d07e
Nyakio-Eva <evalkariuki@gmail.com>
2024-03-03 00:06:06 +0300

    completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("lucy")    
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+    return "Hello, programmer!"
+greet_with_default("Eva")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+print(add(3,5))
 
 def halve(number):
-    pass
+    return number / 2
+print(int(halve(8)))
+
